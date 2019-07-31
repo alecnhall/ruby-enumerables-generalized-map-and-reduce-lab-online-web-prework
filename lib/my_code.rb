2264225d6a4d287d542324e8c0b27c0b645fdd7f
Alec Hall <alecnhall@gmail.com>
2019-07-31 21:45:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,10 +2,8 @@ def map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    yield new.push(source_array[i])
+    new.push(source_array[i])
     i += 1
   end
   return new
 end
-
-map(source_array) { |e| * -1 }
