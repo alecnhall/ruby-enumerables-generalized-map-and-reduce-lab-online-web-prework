bb6eba51ac2b50025831c95c6f4af30bf66f6819
Alec Hall <alecnhall@gmail.com>
2019-07-31 22:05:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce_to_total(source_array, starting_point = 0)
   i = 0
   sum = starting_point
   while i < source_array.length do
-    sum += source_array[yield i]
+    sum += yield source_array[i]
     i += 1
   end
   return sum
