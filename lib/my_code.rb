0380ae7460e3cca6a21f896e576cd25380c4ebb5
Alec Hall <alecnhall@gmail.com>
2019-07-31 20:22:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,1 @@
-source_array = [1, 2, 3, -9]
-def map(source_array)
-  source_array.map {|n| n * -1}
-end
-def map_double(source_array)
-  source_array.map {|n| n * 2}
-end
-def map_to_square(source_array)
-  source_array.map (|n| n ** 2)
-end
+
