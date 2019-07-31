f20464bdf20d370894a12a5f0b5c7c269d7fd939
Alec Hall <alecnhall@gmail.com>
2019-07-31 22:20:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
-source_array = [1, 2, 4, -9]
+array = [1, 2, 4, -9]
 def map(source_array)
   new = []
   i = 0
@@ -7,4 +7,25 @@ def map(source_array)
     i += 1
   end
   return new
+end
+
+def reduce(source_array, starting_point = 0)
+  i = 0
+  sum = starting_point
+  while i < source_array.length do
+    sum += yield source_array[i]
+    i += 1
+  end
+  return sum
+end
+
+def reduce(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == false
+      return yield false
+    end
+    i += 1
+  end
+  return yield true
 end
