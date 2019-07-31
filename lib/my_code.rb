0af29fb1d060e99ff880d78edfc88497c6c35275
Alec Hall <alecnhall@gmail.com>
2019-07-31 22:11:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,4 @@
+
 def map(source_array)
   new = []
   i = 0
@@ -12,8 +13,19 @@ def reduce(source_array, starting_point = 0)
   i = 0
   sum = starting_point
   while i < source_array.length do
-    sum += source_array[i]
+    sum += yield source_array[i]
     i += 1
   end
   return sum
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == false
+      return yield false
+    end
+    i += 1
+  end
+  return yield true
 end
