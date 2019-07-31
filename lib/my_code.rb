eb56c7c4ff3577d56ee698f88147cf94206187e6
Alec Hall <alecnhall@gmail.com>
2019-07-31 22:15:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,25 +9,4 @@ def map(source_array)
   return new
 end
 
-map(array){ |b| b * -1}
 
-def reduce(source_array, starting_point = 0)
-  i = 0
-  sum = starting_point
-  while i < source_array.length do
-    sum += yield source_array[i]
-    i += 1
-  end
-  return sum
-end
-
-def reduce_to_all_true(source_array)
-  i = 0
-  while i < source_array.length do
-    if source_array[i] == false
-      return yield false
-    end
-    i += 1
-  end
-  return yield true
-end
