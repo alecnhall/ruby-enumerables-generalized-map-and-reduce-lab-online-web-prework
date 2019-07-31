45c1cf706aab84caebb51f6618371b4e6a75bb77
Alec Hall <alecnhall@gmail.com>
2019-07-31 22:07:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map(source_array)
   return new
 end
 
-def reduce_to_total(source_array, starting_point = 0)
+def reduce(source_array, starting_point = 0)
   i = 0
   sum = starting_point
   while i < source_array.length do
